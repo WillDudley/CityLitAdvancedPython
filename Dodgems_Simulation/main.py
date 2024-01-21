ed80d8a35b882f4dc5f63389707fcb6f6d483f1a
WillDudley <muddlydudley@gmail.com>
2024-01-21 17:44:56 +0000

    if name==main

--- Dodgems_Simulation/main.py
+++ Dodgems_Simulation/main.py
@@ -2,7 +2,7 @@ from Arena import Arena
 
 arena = Arena(arena_size=5, n_dodgems=5, time_limit=1000)
 
-while not arena.terminated:
-    arena.step()
 
-    # Current bug is remaining dodgem destroys itself
+if __name__ == "__main__":
+    while not arena.terminated:
+        arena.step()
