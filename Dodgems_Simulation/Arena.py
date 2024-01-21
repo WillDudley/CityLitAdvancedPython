c7b5d0ac833e52cb496ffee2678d6b441a14ec14
WillDudley <muddlydudley@gmail.com>
2024-01-21 15:18:02 +0000

    fixed initial position and render erasing bugs

--- Dodgems_Simulation/Arena.py
+++ Dodgems_Simulation/Arena.py
@@ -15,25 +15,34 @@ class Arena:
         self.time_step = 0
 
         for i in list(range(n_dodgems)):
-            self.add_dodgem(i + 1)
+            self.add_dodgem(i + 1, False)
 
-    def add_dodgem(self, dodgem_id):
+        self._render()
+
+    def add_dodgem(self, dodgem_id, random_start=True):
         """
         Create a new dodgem, add it to the list of dodgems, and place it in a random square in the arena
         :return:
         """
 
-        # Logic to check we are not adding a dodgem on top of another dodgem
-        dodgem_unassigned = True
+        if random_start:
+            # Logic to check we are not adding a dodgem on top of another dodgem
+            dodgem_unassigned = True
 
-        while dodgem_unassigned:
-            x_coordinate, y_coordinate = np.random.randint(0, self.arena_size, 2)
-            dodgem_conflict = False
-            for dodgem in self.dodgems:
-                if x_coordinate == dodgem.current_location[0] and y_coordinate == dodgem.current_location[1]:
-                    dodgem_conflict = True
-            if not dodgem_conflict:
-                dodgem_unassigned = False
+            while dodgem_unassigned:
+                x_coordinate, y_coordinate = np.random.randint(0, self.arena_size, 2)
+                dodgem_conflict = False
+                for dodgem in self.dodgems:
+                    if x_coordinate == dodgem.current_location[0] and y_coordinate == dodgem.current_location[1]:
+                        dodgem_conflict = True
+                if not dodgem_conflict:
+                    dodgem_unassigned = False
+        else:
+            x_coordinate = -dodgem_id % self.arena_size
+            y_coordinate = dodgem_id# % self.arena_size
+
+        assert 0 <= x_coordinate < self.arena_size, f"Initial x coordinate for dodgem {dodgem_id} out of bounds!"
+        assert 0 <= y_coordinate < self.arena_size, f"Initial y coordinate for dodgem {dodgem_id} out of bounds!"
 
         self.dodgems.append(Dodgem(hit_points=1,
                                    speed=1,
@@ -54,13 +63,12 @@ class Arena:
         # Move dodgems if they are alive
         for dodgem in self.dodgems:
             if dodgem.alive:
-                self.arena[dodgem.current_location[0], dodgem.current_location[1]] = 0
 
-                # print(f"current location for dodgem {dodgem.dodgem_id}: {dodgem.current_location[0]}, {dodgem.current_location[1]}")
+                # replace old position with 0 UNLESS a new dodgem has already entered the space
+                if self.arena[dodgem.current_location[0], dodgem.current_location[1]] == dodgem.dodgem_id:
+                    self.arena[dodgem.current_location[0], dodgem.current_location[1]] = 0
 
                 dodgem.step()
-
-                # print(f"updated location for dodgem {dodgem.dodgem_id}: {dodgem.current_location[0]}, {dodgem.current_location[1]}")
 
                 # account for collision with wall
                 for i in [0, 1]:
@@ -69,18 +77,16 @@ class Arena:
                     elif dodgem.current_location[i] < 0:
                         dodgem.current_location[i] += 1
 
-                # print(f"updated location for dodgem {dodgem.dodgem_id} after collision check: {dodgem.current_location[0]}, {dodgem.current_location[1]}")
-
-                # print(self.arena)
+                # replace new position with dodgem_id - crash logic is handled later
                 self.arena[dodgem.current_location[0], dodgem.current_location[1]] = dodgem.dodgem_id
-            # print(self.arena)
 
         # Assess for collisions
-        coordinate_list = [tuple(dodgem.current_location) for dodgem in self.dodgems]
+        coordinate_list = [tuple(dodgem.current_location) if dodgem.alive else None for dodgem in self.dodgems]
+        print([d.alive for d in self.dodgems])
+        print(f"Coordinate list: {coordinate_list}")
 
         print(sorted(list_duplicates(coordinate_list)))
         for dup in sorted(list_duplicates(coordinate_list)):  # https://stackoverflow.com/a/5419576
-            # there is a bug here where dead dodgems persist in dup, meaning that any dodgem that collides with a square where another dodgem died is destroyed
             for i in dup[1]:
                 if self.dodgems[i].alive:
                     self.dodgems[i].alive = False
